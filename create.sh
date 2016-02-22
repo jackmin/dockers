3115de50e3f8d0699bb3761136a5607001849ef9
Jack MIN <minxiaoyu@gmail.com>
2016-02-22 13:19:03 +0800

    Remove docker in docker function
    DinD looks like suck the ELX environment

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker create --name=work --hostname=work --privileged -v /lib/:/lib/ -v /usr/lib/:/usr/lib/ -v /usr/bin/docker:/bin/docker -v /var/run/docker.sock:/var/run/docker.sock exiumin/devenv
+docker create --name=work --hostname=work --privileged -v exiumin_data:/exiumin exiumin/devenv
