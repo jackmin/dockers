65c600b1d5cfda732daf3e66921dfebea22bad5b
Jack MIN <minxiaoyu@gmail.com>
2016-02-23 10:33:38 +0800

    Add default workdir

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker create --name=work --hostname=work --privileged -v exiumin_data:/exiumin exiumin/devenv
+docker create -w /exiumin/src --name=work --hostname=work --privileged -v exiumin_data:/exiumin exiumin/devenv
